6e5b7dd8641adad2d637dc31a55f114439dfc21f
junggu <junggu.ji.dev@gmail.com>
2020-07-09 14:02:28 +0900

    type : Test
    getNumbers test case 작성

--- PS/docfriends_ps/src/test/com/docfriends/junggu/task/SolutionTest.java
+++ PS/docfriends_ps/src/test/com/docfriends/junggu/task/SolutionTest.java
@@ -3,6 +3,10 @@ package com.docfriends.junggu.task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
@@ -19,5 +23,28 @@ class SolutionTest {
         assertEquals(0, test.solution("1"));
         assertEquals(1, test.solution("100a2"));
         assertEquals(10, test.solution("a0a1a2a3a4a5a6a7a8a9a10"));
+    }
+
+    @Test
+    void getNumbers() {
+        int[] expected = new int[] {23, 4, 56, 78, 9, 12, 34, 5, 12, 45, 67, 89, 99, 1, 123, 456, 567, 768, 890, 67, 456, 345, 234, 23, 239};
+        int[] actual = test.getNumbers("ab23c4d56e78f9g12h34i5j12k45l67n89m99o1k123p456q567r768s890t67u456v345w234x23y239z".toCharArray()).stream().mapToInt(i -> i).toArray();
+
+        assertArrayEquals(expected, actual);
+
+        int[] expected1 = new int[] {1};
+        int[] actual1 = test.getNumbers("1".toCharArray()).stream().mapToInt(i -> i).toArray();
+
+        assertArrayEquals(expected1, actual1);
+
+        int[] expected2 = new int[] {100, 2};
+        int[] actual2 = test.getNumbers("100a2".toCharArray()).stream().mapToInt(i -> i).toArray();
+
+        assertArrayEquals(expected2, actual2);
+
+        int[] expected3 = new int[] {0,1,2,3,4,5,6,7,8,9,10};
+        int[] actual3 = test.getNumbers("a0a1a2a3a4a5a6a7a8a9a10".toCharArray()).stream().mapToInt(i -> i).toArray();
+
+        assertArrayEquals(expected3, actual3);
     }
 }
