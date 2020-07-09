68a4beb6ede59d7ffff1c565c61af0c6679e7b21
junggu <junggu.ji.dev@gmail.com>
2020-07-09 13:41:04 +0900

    type : Feat
    문자열에서 숫자들만 추출해서 List로 만드는 getNumbers 메서드 추가

--- PS/docfriends_ps/src/main/com/docfriends/junggu/task/Solution.java
+++ PS/docfriends_ps/src/main/com/docfriends/junggu/task/Solution.java
@@ -1,9 +1,38 @@
 package com.docfriends.junggu.task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+    private static final char BEGIN_ASCII = 48;
+    private static final char END_ASCII = 57;
 
     public int solution(String q) {
+        char[] charArray = q.toCharArray();
+
+        List<Integer> numbers = getNumbers(charArray);
 
         return 0;
+    }
+
+    public List<Integer> getNumbers(char[] charArray) {
+        List<Integer> numbers = new ArrayList<>();
+        StringBuilder number = new StringBuilder();
+
+        for (char ch : charArray) {
+            boolean isNumber = false;
+
+            if (ch >= BEGIN_ASCII && ch <= END_ASCII) {
+                isNumber = true;
+                number.append(ch);
+            }
+
+            if (!isNumber && number.length() != 0) {
+                numbers.add(Integer.parseInt(number.toString()));
+                number.setLength(0);
+            }
+        }
+
+        return numbers;
     }
 }
