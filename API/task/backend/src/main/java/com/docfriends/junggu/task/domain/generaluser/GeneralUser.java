0344912e1058853174317f17de7e17eb61f0837e
junggu <junggu.ji.dev@gmail.com>
2020-07-10 22:01:19 +0900

    type : Feat
    일반 유저 ID DB에 설정한대로 자동 생성되도록 추가

--- API/task/backend/src/main/java/com/docfriends/junggu/task/domain/generaluser/GeneralUser.java
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/domain/generaluser/GeneralUser.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @AllArgsConstructor
@@ -12,6 +15,8 @@ import javax.persistence.Entity;
 @Entity
 public class GeneralUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String userId;
     private String password;
