ec5500f3ecfab52c3fe4325ed51e5dda7598ab4a
junggu <junggu.ji.dev@gmail.com>
2020-07-11 13:07:31 +0900

    type : Feat
    main 화면을 위한 jqpl 추가

--- API/task/backend/src/main/java/com/docfriends/junggu/task/domain/question/QuestionRepository.java
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/domain/question/QuestionRepository.java
@@ -1,11 +1,32 @@
 package com.docfriends.junggu.task.domain.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    @Query(value = ""
+            + "SELECT                       "
+            + "    q.title                  "
+            + "    , q.CONTENT              "
+            + "    , q.tag                  "
+            + "    , q.create_date          "
+            + "    , a.content              "
+            + "    , a.create_date          "
+            + "    , d.name                 "
+            + "    , h.name                 "
+            + "    , h.address              "
+            + "    , h.website_url          "
+            + "FROM                         "
+            + "    Question q               "
+            + "JOIN Answer a                "
+            + "    WHERE q.id = a.questionId"
+            + "JOIN Doctor d                "
+            + "    WHERE a.doctorId = d.id  "
+            + "JOIN Hospital h              "
+            + "    WHERE d.hospitalId = h.id")
     List<Object[]> findMainList();
 }
