187eda51d46068e6efe2e9726f65d4f6ca739d64
junggu <junggu.ji.dev@gmail.com>
2020-07-11 20:53:11 +0900

    type : Test
    날짜 localdatetime으로 변경

--- API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
+++ API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,16 +24,20 @@ class QuestionRepositoryTest {
     @Test
     void findMainList() {
         //given
+        LocalDateTime time = LocalDateTime.of(2020, 07,11,19,51,04);
+        LocalDateTime time1 = LocalDateTime.of(2020, 07,11,19,51,05);
+        LocalDateTime time2 = LocalDateTime.of(2020, 07,11,19,52,28);
+
         List<Object[]> given = Arrays.asList(
-                new Object[] {"질문글 제목 01",	"질문내용 01",	"tag 01",	    "2020-07-11T19:51:04",	"답글답글답글 1",	"2020-07-11T19:52:28",	"의사 1",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 01",	"질문내용 01",	"tag 01",	"2020-07-11T19:51:04",	"답글답글답글 2",	"2020-07-11T19:52:28",	"의사 1",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 03",	"질문내용 03",	"tag 03",	"2020-07-11T19:51:04",	"답글답글답글 3",	"2020-07-11T19:52:28",	"의사 3",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 04",	"질문내용 04",	"tag 04",	"2020-07-11T19:51:04",	"답글답글답글 4",	"2020-07-11T19:52:28",	"의사 4",	"병원 2",	"부산 2",	"website_url 2"}
-                , new Object[] {"질문글 제목 05",	"질문내용 05",	"tag 05",	"2020-07-11T19:51:04",	"답글답글답글 5",	"2020-07-11T19:52:28",	"의사 4",	"병원 2",	"부산 2",	"website_url 2"}
-                , new Object[] {"질문글 제목 06",	"질문내용 06",	"tag 06",	"2020-07-11T19:51:05",	"답글답글답글 6",	"2020-07-11T19:52:28",	"의사 2",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 07",	"질문내용 07",	"tag 07",	"2020-07-11T19:51:05",	"답글답글답글 7",	"2020-07-11T19:52:28",	"의사 2",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 08",	"질문내용 08",	"tag 08",	"2020-07-11T19:51:05",	"답글답글답글 8",	"2020-07-11T19:52:28",	"의사 1",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 09",	"질문내용 09",	"tag 09",	"2020-07-11T19:51:05",	"답글답글답글 9",	"2020-07-11T19:52:28",	"의사 3",	"병원 1",	"서울시 1",	"website_url 1"}
+                new Object[] {"질문글 제목 01",	"질문내용 01",	"tag 01",	    time,	"답글답글답글 1",	time2,	"의사 1",	"병원 1",	"서울시 1",	"website_url 1"}
+                , new Object[] {"질문글 제목 01",	"질문내용 01",	"tag 01",	time,	"답글답글답글 2",	time2,	"의사 1",	"병원 1",	"서울시 1",	"website_url 1"}
+                , new Object[] {"질문글 제목 03",	"질문내용 03",	"tag 03",	time,	"답글답글답글 3",	time2,	"의사 3",	"병원 1",	"서울시 1",	"website_url 1"}
+                , new Object[] {"질문글 제목 04",	"질문내용 04",	"tag 04",	time,	"답글답글답글 4",	time2,	"의사 4",	"병원 2",	"부산 2",	"website_url 2"}
+                , new Object[] {"질문글 제목 05",	"질문내용 05",	"tag 05",	time,	"답글답글답글 5",	time2,	"의사 4",	"병원 2",	"부산 2",	"website_url 2"}
+                , new Object[] {"질문글 제목 06",	"질문내용 06",	"tag 06",	time1,	"답글답글답글 6",	time2,	"의사 2",	"병원 1",	"서울시 1",	"website_url 1"}
+                , new Object[] {"질문글 제목 07",	"질문내용 07",	"tag 07",	time1,	"답글답글답글 7",	time2,	"의사 2",	"병원 1",	"서울시 1",	"website_url 1"}
+                , new Object[] {"질문글 제목 08",	"질문내용 08",	"tag 08",	time1,	"답글답글답글 8",	time2,	"의사 1",	"병원 1",	"서울시 1",	"website_url 1"}
+                , new Object[] {"질문글 제목 09",	"질문내용 09",	"tag 09",	time1,	"답글답글답글 9",	time2,	"의사 3",	"병원 1",	"서울시 1",	"website_url 1"}
         );
 
         List<String> columns = new ArrayList<>();
