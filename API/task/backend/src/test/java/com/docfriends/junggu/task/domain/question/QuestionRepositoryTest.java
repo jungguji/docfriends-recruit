e9ac1abab00e10c3837b26188c609edd2e9ed149
junggu <junggu.ji.dev@gmail.com>
2020-07-11 20:18:58 +0900

    type : Test
    Test case 작성

--- API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
+++ API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
@@ -1,20 +1,15 @@
 package com.docfriends.junggu.task.domain.question;
 
-import com.docfriends.junggu.task.domain.answer.Answer;
-import com.docfriends.junggu.task.domain.doctor.Doctor;
-import com.docfriends.junggu.task.domain.generaluser.GeneralUser;
-import com.docfriends.junggu.task.domain.hospital.Hospital;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.time.LocalDateTime;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -30,5 +25,7 @@ class QuestionRepositoryTest {
         //when
         List<Object[]> list = questionRepository.findMainList();
         //than
+        assertEquals(9, list.size());
+        assertEquals(10, list.get(0).length);
     }
 }
