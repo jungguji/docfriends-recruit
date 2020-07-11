8c28a9fda1877c992049f38a721c5a6fae6c7aeb
junggu <junggu.ji.dev@gmail.com>
2020-07-11 13:07:52 +0900

    type : Test
    findMainList test

--- API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
+++ API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
@@ -1,22 +1,32 @@
 package com.docfriends.junggu.task.domain.question;
 
+import com.docfriends.junggu.task.domain.answer.Answer;
+import com.docfriends.junggu.task.domain.doctor.Doctor;
+import com.docfriends.junggu.task.domain.generaluser.GeneralUser;
+import com.docfriends.junggu.task.domain.hospital.Hospital;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@TestPropertySource("classpath:application-test.properties")
 class QuestionRepositoryTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Autowired
+    QuestionRepository questionRepository;
 
     @Test
     void findMainList() {
+        //given
+        //when
+        List<Object[]> list = questionRepository.findMainList();
+        //than
     }
 }
