c1b418cbfed78b350320ff33509d8f6d073d04b0
junggu <junggu.ji.dev@gmail.com>
2020-07-12 15:35:18 +0900

    type : Feat
    Main List를 return하는 getMainList 메서드 생성

--- API/task/backend/src/main/java/com/docfriends/junggu/task/service/QuestionService.java
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/service/QuestionService.java
@@ -1,10 +1,13 @@
 package com.docfriends.junggu.task.service;
 
 import com.docfriends.junggu.task.domain.question.QuestionDTO;
+import com.docfriends.junggu.task.domain.question.QuestionDTO.MainView;
 import com.docfriends.junggu.task.domain.question.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +17,21 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
-    public List<QuestionDTO.MainList> getMainList() {
-        return new ArrayList<>();
+    public List<QuestionDTO.MainView> getMainList() {
+        List<Object[]> mainList = questionRepository.findMainList();
+
+        List<QuestionDTO.MainView> convertMainList = new ArrayList<>();
+
+        for (Object[] objArray : mainList) {
+            String title = (String) objArray[0];
+            String content = (String) objArray[1];
+            String tag = (String) objArray[2];
+            LocalDate createDate = ((LocalDateTime) objArray[3]).toLocalDate();
+            Long answerCount = (Long) objArray[4];
+
+            convertMainList.add(new MainView(title, content, tag, createDate, answerCount));
+        }
+
+        return convertMainList;
     }
 }
