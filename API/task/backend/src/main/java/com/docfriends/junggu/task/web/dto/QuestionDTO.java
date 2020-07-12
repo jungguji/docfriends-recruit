78f4ba4ec452807c76424e7c06e86095d03dc755
junggu <junggu.ji.dev@gmail.com>
2020-07-12 20:52:51 +0900

    type : Refactor
    답글 정보들 DTO로 따로 뺌

--- API/task/backend/src/main/java/com/docfriends/junggu/task/web/dto/QuestionDTO.java
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/web/dto/QuestionDTO.java
@@ -1,12 +1,13 @@
 package com.docfriends.junggu.task.web.dto;
 
+import com.docfriends.junggu.task.web.dto.AnswerDTO.ConsultDetail;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
-
+import java.util.List;
 
 public class QuestionDTO {
 
@@ -29,10 +30,6 @@ public class QuestionDTO {
         private final String content;
         private String tag;
         private final LocalDate createDate;
-        private final String answerContent;
-        private final LocalDate answerCreateDate;
-        private final String doctorName;
-        private final String hospitalName;
-        private final String hospitalAddress;
+        private final List<AnswerDTO.ConsultDetail> answers;
     }
 }
