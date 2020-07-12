c8a36591cb7c978545cc9a9cdf28791e3f40bc55
junggu <junggu.ji.dev@gmail.com>
2020-07-12 16:33:20 +0900

    type : Feat
    response 전송 시 한글 꺠지지 않도록 UTF-8 설정 추가

--- API/task/backend/src/main/java/com/docfriends/junggu/task/TaskApplication.java
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/TaskApplication.java
@@ -2,8 +2,14 @@ package com.docfriends.junggu.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 @PropertySources(
@@ -15,4 +21,16 @@ public class TaskApplication {
 		SpringApplication.run(TaskApplication.class, args);
 	}
 
+	@Bean
+	public HttpMessageConverter<String> responseBodyConverter() {
+		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+	}
+
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
 }
