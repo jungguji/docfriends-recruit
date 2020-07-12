252ae965607370f91d8e53861c1fcc6596f019eb
junggu <junggu.ji.dev@gmail.com>
2020-07-12 13:05:35 +0900

    type : Test
    숫자는 Long형으로 return 되므로 테스트 데이터 Long형으로 변경

--- API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
+++ API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
@@ -28,14 +28,14 @@ class QuestionRepositoryTest {
         LocalDateTime time1 = LocalDateTime.of(2020, 07,11,19,51,05);
 
         List<Object[]> given = Arrays.asList(
-                new Object[]   {"질문글 제목 01",	"질문내용 01",	"tag 01",	time, 2}
-                , new Object[] {"질문글 제목 03",	"질문내용 03",	"tag 03",	time, 1}
-                , new Object[] {"질문글 제목 04",	"질문내용 04",	"tag 04",	time, 1}
-                , new Object[] {"질문글 제목 05",	"질문내용 05",	"tag 05",	time, 1}
-                , new Object[] {"질문글 제목 06",	"질문내용 06",	"tag 06",	time1, 1}
-                , new Object[] {"질문글 제목 07",	"질문내용 07",	"tag 07",	time1, 1}
-                , new Object[] {"질문글 제목 08",	"질문내용 08",	"tag 08",	time1, 1}
-                , new Object[] {"질문글 제목 09",	"질문내용 09",	"tag 09",	time1, 1}
+                new Object[]   {"질문글 제목 01",	"질문내용 01",	"tag 01",	time, 2L}
+                , new Object[] {"질문글 제목 03",	"질문내용 03",	"tag 03",	time, 1L}
+                , new Object[] {"질문글 제목 04",	"질문내용 04",	"tag 04",	time, 1L}
+                , new Object[] {"질문글 제목 05",	"질문내용 05",	"tag 05",	time, 1L}
+                , new Object[] {"질문글 제목 06",	"질문내용 06",	"tag 06",	time1, 1L}
+                , new Object[] {"질문글 제목 07",	"질문내용 07",	"tag 07",	time1, 1L}
+                , new Object[] {"질문글 제목 08",	"질문내용 08",	"tag 08",	time1, 1L}
+                , new Object[] {"질문글 제목 09",	"질문내용 09",	"tag 09",	time1, 1L}
         );
 
         List<String> columns = new ArrayList<>();
