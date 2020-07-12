3c67c7726ba8ae687b38fa7b3f38b7cb627542be
junggu <junggu.ji.dev@gmail.com>
2020-07-12 13:29:31 +0900

    type : Test
    변경된 DTO 테스트 케이스 수정

--- API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionDTOTest.java
+++ API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionDTOTest.java
@@ -1,5 +1,6 @@
 package com.docfriends.junggu.task.domain.question;
 
+import com.docfriends.junggu.task.domain.question.QuestionDTO.MainList;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -15,19 +16,16 @@ class QuestionDTOTest {
         String content = "내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용";
         String tag = "태그태그, 한방";
         LocalDateTime createDate = LocalDateTime.now();
+        Long answerCount = 2L;
 
         //when
-        QuestionDTO dto = QuestionDTO.builder()
-                .title(title)
-                .content(content)
-                .tag(tag)
-                .createDate(createDate)
-                .build();
+        QuestionDTO.MainList dto = new MainList(title,	content,	tag,	createDate, answerCount);
 
         //than
         assertEquals(title, dto.getTitle());
         assertEquals(content, dto.getContent());
         assertEquals(tag, dto.getTag());
         assertEquals(createDate, dto.getCreateDate());
+        assertEquals(answerCount, dto.getAnswerCount());
     }
 }
