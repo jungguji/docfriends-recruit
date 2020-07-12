915b246a23591e5bc98c086aefd5103f166e5433
junggu <junggu.ji.dev@gmail.com>
2020-07-12 22:42:03 +0900

    type : Feat
    디테일뷰 data view로 리턴 할 controller findConsultDetail 메서드 생성

--- API/task/backend/src/main/java/com/docfriends/junggu/task/web/QuestionController.java
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/web/QuestionController.java
@@ -1,10 +1,11 @@
 package com.docfriends.junggu.task.web;
 
-import com.docfriends.junggu.task.domain.question.QuestionDTO;
+import com.docfriends.junggu.task.web.dto.QuestionDTO;
 import com.docfriends.junggu.task.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -24,5 +25,11 @@ public class QuestionController {
     @ResponseBody
     public List<QuestionDTO.MainView> findQuestionMainList() {
         return questionService.getMainList();
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    @ResponseBody
+    public QuestionDTO.ConsultDetail findConsultDetail(@PathVariable("id") int id) {
+        return questionService.findConsultDetail(id);
     }
 }
