5fc58d51f9a265b56e4aebe772c2e5a7a191b302
junggu <junggu.ji.dev@gmail.com>
2020-07-12 13:02:59 +0900

    type : Test
    수정된 메서드 테스트 케이스 작성

--- API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
+++ API/task/backend/src/test/java/com/docfriends/junggu/task/domain/question/QuestionRepositoryTest.java
@@ -26,30 +26,24 @@ class QuestionRepositoryTest {
         //given
         LocalDateTime time = LocalDateTime.of(2020, 07,11,19,51,04);
         LocalDateTime time1 = LocalDateTime.of(2020, 07,11,19,51,05);
-        LocalDateTime time2 = LocalDateTime.of(2020, 07,11,19,52,28);
 
         List<Object[]> given = Arrays.asList(
-                new Object[] {"질문글 제목 01",	"질문내용 01",	"tag 01",	    time,	"답글답글답글 1",	time2,	"의사 1",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 01",	"질문내용 01",	"tag 01",	time,	"답글답글답글 2",	time2,	"의사 1",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 03",	"질문내용 03",	"tag 03",	time,	"답글답글답글 3",	time2,	"의사 3",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 04",	"질문내용 04",	"tag 04",	time,	"답글답글답글 4",	time2,	"의사 4",	"병원 2",	"부산 2",	"website_url 2"}
-                , new Object[] {"질문글 제목 05",	"질문내용 05",	"tag 05",	time,	"답글답글답글 5",	time2,	"의사 4",	"병원 2",	"부산 2",	"website_url 2"}
-                , new Object[] {"질문글 제목 06",	"질문내용 06",	"tag 06",	time1,	"답글답글답글 6",	time2,	"의사 2",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 07",	"질문내용 07",	"tag 07",	time1,	"답글답글답글 7",	time2,	"의사 2",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 08",	"질문내용 08",	"tag 08",	time1,	"답글답글답글 8",	time2,	"의사 1",	"병원 1",	"서울시 1",	"website_url 1"}
-                , new Object[] {"질문글 제목 09",	"질문내용 09",	"tag 09",	time1,	"답글답글답글 9",	time2,	"의사 3",	"병원 1",	"서울시 1",	"website_url 1"}
+                new Object[]   {"질문글 제목 01",	"질문내용 01",	"tag 01",	time, 2}
+                , new Object[] {"질문글 제목 03",	"질문내용 03",	"tag 03",	time, 1}
+                , new Object[] {"질문글 제목 04",	"질문내용 04",	"tag 04",	time, 1}
+                , new Object[] {"질문글 제목 05",	"질문내용 05",	"tag 05",	time, 1}
+                , new Object[] {"질문글 제목 06",	"질문내용 06",	"tag 06",	time1, 1}
+                , new Object[] {"질문글 제목 07",	"질문내용 07",	"tag 07",	time1, 1}
+                , new Object[] {"질문글 제목 08",	"질문내용 08",	"tag 08",	time1, 1}
+                , new Object[] {"질문글 제목 09",	"질문내용 09",	"tag 09",	time1, 1}
         );
 
         List<String> columns = new ArrayList<>();
         columns.add("questionTitle");
         columns.add("questionContent");
         columns.add("questionTag");
-        columns.add("answerContent");
-        columns.add("answerCreateDate");
-        columns.add("doctorName");
-        columns.add("hospitalName");
-        columns.add("hospitalAddress");
-        columns.add("hospitalWebsiteUrl");
+        columns.add("questionCreateDate");
+        columns.add("answercCount");
 
         List<Map<String, Object>> givenList = convertMapList(given, columns);
 
@@ -83,6 +77,4 @@ class QuestionRepositoryTest {
         }
         return mapList;
     }
-
-
 }
