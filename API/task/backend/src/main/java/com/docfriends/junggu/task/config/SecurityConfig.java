1db6823651e735868301cb185faf1978d1842b5b
junggu <junggu.ji.dev@gmail.com>
2020-07-13 15:02:02 +0900

    type : Feat
    로그인 시 비밀번호 인코딩하지 않도록 처리

--- API/task/backend/src/main/java/com/docfriends/junggu/task/config/SecurityConfig.java
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.docfriends.junggu.task.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) {
@@ -21,7 +25,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(filter, CsrfFilter.class);
 
         http.authorizeRequests()
+                .antMatchers("/main").authenticated()
+                .antMatchers("/detail/**").authenticated()
                 .antMatchers("/**").permitAll()
                 .and();
+    }
+
+    @Bean
+    public PasswordEncoder noOpPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
