27b59b162906ec219da05c825780609a4a923dd3
junggu <junggu.ji.dev@gmail.com>
2020-07-13 14:37:32 +0900

    type : Test
    스프링시큐리티 Test 추가

--- API/task/backend/src/test/java/com/docfriends/junggu/task/web/QuestionControllerTest.java
+++ API/task/backend/src/test/java/com/docfriends/junggu/task/web/QuestionControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
+@WithMockUser
 class QuestionControllerTest {
 
     @Autowired
