fc6c91732ec594da0db6cac6610cf18bf9866a05
junggu <junggu.ji.dev@gmail.com>
2020-07-13 14:03:24 +0900

    type : Feat
    스프링시큐리티 추가

---
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.docfriends.junggu.task.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter, CsrfFilter.class);
+
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and();
+    }
+}
