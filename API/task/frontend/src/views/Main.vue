1d527624be04d87deecc1f451a5239dbca2b10e7
junggu <junggu.ji.dev@gmail.com>
2020-07-13 16:36:46 +0900

    type : Feat
    메인 리스트를 위한 Main Vue 추가

---
+++ API/task/frontend/src/views/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <MainList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainList from "@/components/MainList.vue";
+
+export default {
+  name: "Main",
+  components: {
+    MainList
+  }
+};
+</script>
