e9ee75b1c867d7c6dd86aa116735683a49dc4341
junggu <junggu.ji.dev@gmail.com>
2020-07-13 22:39:12 +0900

    type : Feat
    detail view 용 vue 생성

---
+++ API/task/frontend/src/components/Consult.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="detail">
+    Consult.vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConsultDetail',
+  data: function() {
+    return {
+      id: null,
+      consult: ''
+    }
+  }
+}
+</script>
---
+++ API/task/frontend/src/views/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    Detail.vue
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <ConsultDetail/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ConsultDetail from "@/components/Consult.vue";
+
+export default {
+  name: "Detail",
+  components: {
+    ConsultDetail
+  }
+};
+</script>
