1aa16b174a317b8965035908b5d0124473022b5d
junggu <junggu.ji.dev@gmail.com>
2020-07-15 01:05:24 +0900

    type : Feat
    login url 변경

--- API/task/backend/src/main/java/com/docfriends/junggu/task/web/LoginController.java
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/web/LoginController.java
@@ -2,14 +2,14 @@ package com.docfriends.junggu.task.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login/test")
-    @ResponseBody
+    @GetMapping("/login")
     public String login() {
-        return "success";
+        return "login";
     }
 }
