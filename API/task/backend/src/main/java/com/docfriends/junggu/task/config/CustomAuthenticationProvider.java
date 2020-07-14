b95c395eb0e605147154ae1ded4d71515bd3f977
junggu <junggu.ji.dev@gmail.com>
2020-07-15 01:05:54 +0900

    type : Fix
    if 조건문 반대로 되어 있던것 수정

--- API/task/backend/src/main/java/com/docfriends/junggu/task/config/CustomAuthenticationProvider.java
+++ API/task/backend/src/main/java/com/docfriends/junggu/task/config/CustomAuthenticationProvider.java
@@ -1,7 +1,6 @@
 package com.docfriends.junggu.task.config;
 
 import com.docfriends.junggu.task.service.LoginService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -10,7 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
@@ -26,7 +24,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
             throw new UsernameNotFoundException("아이디 혹은 비밀번호가 올바르지 않습니다.");
         }
 
-        if ((((String) authToken.getCredentials()).equals(user.getPassword()))) {
+        if (!((String) authToken.getCredentials()).equals(user.getPassword())) {
             throw new BadCredentialsException("아이디 혹은 비밀번호가 올바르지 않습니다.");
         }
 
