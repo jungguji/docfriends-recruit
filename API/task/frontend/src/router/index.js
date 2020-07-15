49ac47688fc88a3b7934142623ce10881fafb0b9
junggu <junggu.ji.dev@gmail.com>
2020-07-16 00:10:27 +0900

    type : Feat
    불필요한 링크 제거 및 login 버튼 클릭 시 thymeleaf login page로 이동하도록 수정

--- API/task/frontend/src/router/index.js
+++ API/task/frontend/src/router/index.js
@@ -1,26 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/login",
+    name: "Login",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      window.location = "http://localhost:8080/login"
   },
   {
-    path: "/main",
+    path: "/",
     name: "Main",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Main.vue")
@@ -32,11 +23,12 @@ const routes = [
       import(/* webpackChunkName: "about" */ "@/views/Detail.vue")
   },
   {
-    path: "/login",
-    name: "Login",
+    path: "/logout",
+    name: "Logout",
     component: () =>
-      import(/* webpackChunkName: "about" */ "@/views/Login.vue")
-  }
+      window.location = "http://localhost:8080/logout"
+  },
+  
 ];
 
 const router = new VueRouter({
