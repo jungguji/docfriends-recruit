2af55f566cfcd100b11d56c42beab9e7c144102d
junggu <junggu.ji.dev@gmail.com>
2020-07-15 13:57:54 +0900

    type : Chore
    thymeleaf 적용으로 인해 indexPath 변경

--- API/task/frontend/vue.config.js
+++ API/task/frontend/vue.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
 
 module.exports = {
+    assetsDir: "static",
     outputDir: path.resolve(__dirname, "../backend/src/main/resources/static"),
+    indexPath: "../templates/index.html",
     devServer: {
         proxy: {
             '/' : {
@@ -10,5 +12,10 @@ module.exports = {
                 , changeOrigin: true
             }
         }
-    }
+    },
+    chainWebpack: config => {
+        const svgRule = config.module.rule("svg");
+        svgRule.uses.clear();
+        svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+      }
 }
