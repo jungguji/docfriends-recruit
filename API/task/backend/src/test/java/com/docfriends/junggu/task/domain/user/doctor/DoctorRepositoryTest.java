0dfcf7a9d7d5985575ebedc09e89c381599bf25c
junggu <junggu.ji.dev@gmail.com>
2020-07-16 13:30:44 +0900

    type : Test
    repository test H2 db로 변경

--- API/task/backend/src/test/java/com/docfriends/junggu/task/domain/user/doctor/DoctorRepositoryTest.java
+++ API/task/backend/src/test/java/com/docfriends/junggu/task/domain/user/doctor/DoctorRepositoryTest.java
@@ -4,15 +4,15 @@ import com.docfriends.junggu.task.domain.user.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(value = "classpath:application-test.properties")
 class DoctorRepositoryTest {
 
     @Autowired
@@ -25,6 +25,14 @@ class DoctorRepositoryTest {
         String userId = "user_id 1";
         String pw = "password 1";
         String name = "의사 1";
+
+        Doctor doctor = Doctor.builder()
+                .userId(userId)
+                .password(pw)
+                .name(name)
+                .build();
+
+        doctorRepository.save(doctor);
 
         //when
         User given = doctorRepository.findByUserId(userId);
--- API/task/backend/src/test/java/com/docfriends/junggu/task/domain/user/general/GeneralUserRepositoryTest.java
+++ API/task/backend/src/test/java/com/docfriends/junggu/task/domain/user/general/GeneralUserRepositoryTest.java
@@ -4,15 +4,15 @@ import com.docfriends.junggu.task.domain.user.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource("classpath:application-test.properties")
 class GeneralUserRepositoryTest {
 
     @Autowired
@@ -24,6 +24,14 @@ class GeneralUserRepositoryTest {
         String userId = "user_id 1";
         String pw = "password 1";
         String name = "일반유저 1";
+
+        GeneralUser generalUser = GeneralUser.builder()
+                .userId(userId)
+                .password(pw)
+                .name(name)
+                .build();
+
+        generalUserRepository.save(generalUser);
 
         //when
         User target = generalUserRepository.findByUserId(userId);
